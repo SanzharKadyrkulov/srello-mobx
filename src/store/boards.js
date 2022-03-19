698fdbff1a4145fff34ce6816861d7760a4d5a18
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2022-03-19 11:10:26 +0600

    deploy

--- src/store/boards.js
+++ src/store/boards.js
@@ -1,47 +1,48 @@
-import axios from "axios";
-import { makeAutoObservable } from "mobx";
+import axios from 'axios';
+import { makeAutoObservable } from 'mobx';
+import { API } from '../helpers/consts';
 
 class Board {
-  boards = [];
-  constructor() {
-    makeAutoObservable(this);
-  }
-  async fetchBoards() {
-    try {
-      const { data } = await axios.get("http://localhost:8000/boards");
-      this.boards = data;
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  async addBoard(title) {
-    try {
-      const board = {
-        title,
-        items: [],
-      };
-      await axios.post("http://localhost:8000/boards", board);
-      this.fetchBoards();
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  async deleteBoard(id) {
-    try {
-      await axios.delete(`http://localhost:8000/boards/${id}`);
-      this.fetchBoards();
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  async saveBoard(id, board) {
-    try {
-      await axios.patch(`http://localhost:8000/boards/${id}`, board);
-      this.fetchBoards();
-    } catch (e) {
-      console.log(e);
-    }
-  }
+	boards = [];
+	constructor() {
+		makeAutoObservable(this);
+	}
+	async fetchBoards() {
+		try {
+			const { data } = await axios.get(`${API}/boards`);
+			this.boards = data;
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	async addBoard(title) {
+		try {
+			const board = {
+				title,
+				items: [],
+			};
+			await axios.post(`${API}/boards`, board);
+			this.fetchBoards();
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	async deleteBoard(id) {
+		try {
+			await axios.delete(`${API}/boards/${id}`);
+			this.fetchBoards();
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	async saveBoard(id, board) {
+		try {
+			await axios.patch(`${API}/boards/${id}`, board);
+			this.fetchBoards();
+		} catch (e) {
+			console.log(e);
+		}
+	}
 }
 
 export default new Board();
--- src/store/todo.js
+++ src/store/todo.js
@@ -1,52 +1,53 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable } from 'mobx';
+import { API } from '../helpers/consts';
 
 class Todo {
-  todos = [];
-  constructor() {
-    makeAutoObservable(this);
-  }
-  addTodo(todo) {
-    fetch("http://localhost:8000/todos", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(todo),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        this.todos.push(json);
-      });
-  }
-  removeTodo(id) {
-    fetch(`http://localhost:8000/todos/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    // this.todos = this.todos.filter((todo) => todo.id != id);
-    this.fetchTodos();
-  }
-  completeTodo(t) {
-    fetch(`http://localhost:8000/todos/${t.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ...t, completed: !t.completed }),
-    });
-    // this.todos = this.todos.map((todo) =>
-    //   todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    // );
-    this.fetchTodos();
-  }
-  fetchTodos() {
-    fetch("http://localhost:8000/todos")
-      .then((response) => response.json())
-      .then((json) => {
-        this.todos = [...json];
-      });
-  }
+	todos = [];
+	constructor() {
+		makeAutoObservable(this);
+	}
+	addTodo(todo) {
+		fetch(`${API}/todos`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(todo),
+		})
+			.then((response) => response.json())
+			.then((json) => {
+				this.todos.push(json);
+			});
+	}
+	removeTodo(id) {
+		fetch(`${API}/todos/${id}`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		// this.todos = this.todos.filter((todo) => todo.id != id);
+		this.fetchTodos();
+	}
+	completeTodo(t) {
+		fetch(`${API}/todos/${t.id}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ ...t, completed: !t.completed }),
+		});
+		// this.todos = this.todos.map((todo) =>
+		//   todo.id === id ? { ...todo, completed: !todo.completed } : todo
+		// );
+		this.fetchTodos();
+	}
+	fetchTodos() {
+		fetch(`${API}/todos`)
+			.then((response) => response.json())
+			.then((json) => {
+				this.todos = [...json];
+			});
+	}
 }
 export default new Todo();
